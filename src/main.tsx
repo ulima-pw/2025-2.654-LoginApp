81af8ca4063c7b62c35115bcb3a9cc6c5d3d9021
ProfN409 <ProfN409@lab.aca>
2025-09-19 20:25:57 -0500

    Migracion de html a react terminada

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import LoginPage from './LoginPage'
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <LoginPage />
   </StrictMode>,
 )
