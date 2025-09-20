528e9f722d274d5afef32652881c33532ca995c4
ProfN409 <ProfN409@lab.aca>
2025-09-19 21:45:35 -0500

    Operador ternario

--- src/Imagen.tsx
+++ src/Imagen.tsx
@@ -1,8 +1,15 @@
-const Imagen = () => {
+const Imagen = (props : ImagenProps) => {
     return <div className="mb-3">
-        <img className="w-70" 
-            src="https://upload.wikimedia.org/wikipedia/commons/e/e2/Universidad_de_Lima_logo.svg" />
+        {
+            props.mostrarImagen == true ? 
+                <img className="w-70" src="https://upload.wikimedia.org/wikipedia/commons/e/e2/Universidad_de_Lima_logo.svg" /> : 
+                ""
+        }
     </div>
+}
+
+interface ImagenProps {
+    mostrarImagen : boolean
 }
 
 export default Imagen
