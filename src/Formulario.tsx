9d8a2f7a4796f094cc931582eeda0144d3854f6b
ProfO2204 <ProfO2204@lab.aca>
2025-09-24 21:34:03 -0500

    Componentes de form gestionados por variables de estado

--- src/Formulario.tsx
+++ src/Formulario.tsx
@@ -1,7 +1,28 @@
+import { useState, type ChangeEvent } from "react"
+
 const Formulario = () => {
+    const [username, setUsername] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
+    const handleUsernameOnChange = (e : ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.currentTarget.value)
+    }
+
+    const handlePasswordOnChange = (e : ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }
+
     return <form>
-        <input placeholder={"Ingrese usuario"} className="form-control mb-2" type="text" />
-        <input placeholder="Ingrese password" className="form-control mb-2" type="text" />
+        <input placeholder={"Ingrese usuario"} 
+            className="form-control mb-2" 
+            type="text"
+            value={ username }
+            onChange={ handleUsernameOnChange } />
+        <input placeholder="Ingrese password" 
+            className="form-control mb-2" 
+            type="password"
+            value={ password }
+            onChange={ handlePasswordOnChange } />
         <button className="btn btn-primary w-100" type="button">
             Ingresar
         </button>
