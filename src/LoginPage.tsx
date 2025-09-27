1f43e13107664edc7b104be9e93ed809b08c70ff
ProfN409 <ProfN409@lab.aca>
2025-09-26 20:45:16 -0500

    Terminando de configurar el ruteo utilizando react-router

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react"
 import SeccionCentral from "./SeccionCentral"
+import { Link, useNavigate } from "react-router-dom"
 
 const LoginPage = () => {
     const [resultadoLogin, setResultadoLogin] = useState<boolean | undefined>(undefined)
+    const navigate = useNavigate()
 
     const login = (username : string, password : string) => {
         if (username == "PW" && password == "juler"){
             // Login exitoso
             console.log("Login exitoso")
             setResultadoLogin(true)
+            navigate("/main")  // => MainPage
         }else {
             // Error login
             console.log("Error login")
@@ -22,6 +25,7 @@ const LoginPage = () => {
             <SeccionCentral 
                 onLogin={ login }
                 resultadoLogin={ resultadoLogin }/>
+            <Link to={"/main"}>Ir a MainPage</Link>
         </div>
         <div className="col"></div>
     </div>  
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,15 @@ import LoginPage from './LoginPage'
 
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import MainPage from './MainPage'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LoginPage />
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={ <LoginPage /> }/>
+        <Route path='/main' element={ <MainPage /> }/>
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
