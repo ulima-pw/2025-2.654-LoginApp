289308b3f884211d063dc68531ca142354bd3735
ProfN409 <ProfN409@lab.aca>
2025-09-26 20:01:16 -0500

    Terminando los mensajes

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
 import SeccionCentral from "./SeccionCentral"
 
 const LoginPage = () => {
+    const [resultadoLogin, setResultadoLogin] = useState<boolean>(true)
 
     const login = (username : string, password : string) => {
         if (username == "PW" && password == "juler"){
             // Login exitoso
             console.log("Login exitoso")
+            setResultadoLogin(true)
         }else {
             // Error login
             console.log("Error login")
+            setResultadoLogin(false)
         }
     }
 
@@ -17,11 +21,10 @@ const LoginPage = () => {
         <div className="col">
             <SeccionCentral 
                 onLogin={ login }
-                resultadoLogin={ false }/>
+                resultadoLogin={ resultadoLogin }/>
         </div>
         <div className="col"></div>
     </div>  
-
 }
 
 export default LoginPage
