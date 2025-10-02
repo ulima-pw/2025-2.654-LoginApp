dd6e5344acf4ea8da2502f3d7d5f0a02051c7252
ProfO2204 <ProfO2204@lab.aca>
2025-10-01 21:51:46 -0500

    Login con session y local storage

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -12,10 +12,50 @@ const LoginPage = () => {
             console.log("Login exitoso")
             setResultadoLogin(true)
             navigate("/main")  // => MainPage
+
+            const fechaAnterior = localStorage.getItem("FECHA_LOGIN")
+
+            const cantMilisecs = new Date().getTime().toString()
+
+            if (fechaAnterior != null) {
+                localStorage.setItem("FECHA_ANTERIOR_LOGIN", fechaAnterior)
+            }else {
+                localStorage.setItem("FECHA_ANTERIOR_LOGIN", cantMilisecs)
+            }
+            localStorage.setItem("FECHA_LOGIN", cantMilisecs)
+
+            const dataUsuario = sessionStorage.getItem("USER")
+            if (dataUsuario == null) {
+                const usuario = {
+                    username : username,
+                    cantidadIntentos : 0
+                }
+                sessionStorage.setItem("USER", JSON.stringify(usuario))
+            } else {
+                const usuario = JSON.parse(dataUsuario)
+                usuario.username = username
+
+                sessionStorage.setItem("USER", JSON.stringify(usuario))
+            }
         }else {
             // Error login
             console.log("Error login")
             setResultadoLogin(false)
+
+            const dataUsuario = sessionStorage.getItem("USER")
+            if (dataUsuario == null) {
+                const usuario = {
+                    username : username,
+                    cantidadIntentos : 1
+                }
+                sessionStorage.setItem("USER", JSON.stringify(usuario))
+            } else {
+                const usuario = JSON.parse(dataUsuario)
+                usuario.username = username
+                usuario.cantidadIntentos += 1
+
+                sessionStorage.setItem("USER", JSON.stringify(usuario))
+            }
         }
     }
 
--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,6 +1,18 @@
 import Titulo from "./Titulo"
 
 const MainPage = () => {
+
+    const fechaUltimoLogin = localStorage.getItem("FECHA_ANTERIOR_LOGIN")
+    const fechaLogin = localStorage.getItem("FECHA_LOGIN")
+
+    if (fechaLogin != null && fechaUltimoLogin != null) {
+        const fUltimo = parseInt(fechaUltimoLogin)
+        const fLogin = parseInt(fechaLogin)
+
+        const cantidadSegundos = (fLogin - fUltimo) / 1000
+        console.log(cantidadSegundos)
+    }
+
     return <div>
         <Titulo texto="Main" color="blue"/>
     </div>
