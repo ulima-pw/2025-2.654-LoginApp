33cdfb8c68da843572952781725a998a74483e56
ProfO2204 <ProfO2204@lab.aca>
2025-10-29 21:39:31 -0500

    Seguridad en MainPage

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react"
 import Titulo from "./Titulo"
+import { data, useNavigate } from "react-router-dom"
 
 const MainPage = () => {
+    const navigate = useNavigate()
+    const [texto, setTexto] = useState<string>("")
+
+    const httpProfile = async () => {
+        const dataUsuarioStr = sessionStorage.getItem("USER")
+        if (dataUsuarioStr == null) {
+            // No hay nada en sessionStore
+            // Error de credenciales
+            navigate("/")
+            return
+        }
+        const token = JSON.parse(dataUsuarioStr).token
+
+        const resp = await fetch("http://localhost:5002/profile", {
+            headers : {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+
+        if (resp.status == 200) {
+            const data = await resp.text()
+            setTexto(data)
+        }else {
+            // Error de credenciales
+            navigate("/")
+        }
+    }
 
     const fechaUltimoLogin = localStorage.getItem("FECHA_ANTERIOR_LOGIN")
     const fechaLogin = localStorage.getItem("FECHA_LOGIN")
@@ -13,8 +42,12 @@ const MainPage = () => {
         console.log(cantidadSegundos)
     }
 
+    useEffect( () => {
+        httpProfile()
+    }, [] )
+
     return <div>
-        <Titulo texto="Main" color="blue"/>
+        <Titulo texto={texto} color="blue"/>
     </div>
 }
 
